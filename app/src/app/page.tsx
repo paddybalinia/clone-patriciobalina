208ed79d30419478e2f8c7c37ce880a09d107198
Paddy <paddybalinia@gmail.com>
2024-05-29 10:51:59 -0300

    feat: se agrega el componente de cliente

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,4 @@
-"use client";
-import { useState } from "react";
-import Header from "./components/Header/Header";
-import Nav from "./components/Nav/Nav";
+import ClientWrapper from "./components/ClientWrapper/ClientWrapper";
 import SectionAbout from "./components/SectionAbout/SectionAbout";
 import SectionContact from "./components/SectionContact/SectionContact";
 import SectionExperience from "./components/SectionExperience/SectionExperience";
@@ -9,12 +6,9 @@ import SectionHome from "./components/SectionHome/SectionHome";
 import styles from "./styles/page.module.scss";
 
 export default function Home() {
-  const [isNavVisible, setIsNavVisible] = useState<boolean>(false);
-
   return (
     <main className={styles.main}>
-      <Header setIsNavVisible={setIsNavVisible} />
-      <Nav isVisible={isNavVisible} setIsNavVisible={setIsNavVisible} />
+      <ClientWrapper />
       <SectionHome />
       <SectionAbout />
       <SectionExperience />
